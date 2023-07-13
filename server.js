e960b5cd671cb211072aa363fc1fb65d9c327c91
DariyaDro <116110019+DariyaDro@users.noreply.github.com>
2023-07-13 16:48:48 +0300

    Update server.js

--- server.js
+++ server.js
@@ -24,10 +24,12 @@ app.get('/api/:bb', (req, res) => {
     connection.connect();
     const bird_name=req.params.bb.replace("_", " ");
     console.log(bird_name);
+
     connection.query('select * from bio_taxons where parentId in (select id from bio_taxons where parentId= (select parentId from bio_taxons where id =(select parentId from bio_taxons where id=(select taxonId from bio_taxons_locale where title_ru=?)))) order by RAND() limit 3', [bird_name], function(err, results) {
         if (err) throw err;
         res.json(results);
     });
+
 
     connection.end();
 });
